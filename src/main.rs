4199a1310a0379f4da6b94864851cac4f5a378b6
NamesMark <markfirn@gmail.com>
2023-09-08 01:12:00 +0300

    Generics

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ mod args;
 mod files;
 mod check_roster;
 mod rectangle_struct;
+mod sum_boxes;
 
 fn main () {
     //number_guessing::main();
     // args::main();
     // files::main();
     // check_roster::main();
-    rectangle_struct::main();
+    // rectangle_struct::main();
+    sum_boxes::main();
 }
