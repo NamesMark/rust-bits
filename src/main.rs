d6bdf04e24d6fac84adc26eb714a9c3f535a3693
NamesMark <markfirn@gmail.com>
2023-09-08 00:35:08 +0300

    Structs and methods

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ mod number_guessing;
 mod args;
 mod files;
 mod check_roster;
+mod rectangle_struct;
 
 fn main () {
     //number_guessing::main();
     // args::main();
     // files::main();
-    check_roster::main();
+    // check_roster::main();
+    rectangle_struct::main();
 }
